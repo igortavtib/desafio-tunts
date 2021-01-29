802d910bd03c3d4fc1f02dd74e2f6bfad26b54cb
Igor Tibiriçá <48259810+igortavtib@users.noreply.github.com>
2021-01-29 09:02:17 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const auth = require('./auth/auth');
 const gradesServices = require('./gradesServices');
 
 /**
- * Authroize the request besides Google auth and call
+ * Authroize the request through Google auth and call
  * the updateGrades functions as a callback.
  */
 auth.authorize(credentials, gradesServices.updateGrades);
